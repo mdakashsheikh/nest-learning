d491a99a3f3302a5bdaec7e3b5d333f8d4246c95
MdAkashSheikh <nmakashsheikh@gmail.com>
2024-10-17 10:59:35 +0600

    Validation

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post, Param } from "@nestjs/common";
 import { booksService } from "./books.service";
+import { createDto } from "./dtos/create.dto";
 
 @Controller('book')
 export class booksController {
@@ -7,7 +8,16 @@ export class booksController {
     constructor(private readonly booksService:booksService) {}
 
     @Get('/gether')
-    getBook(): string {
+    getBook(@Param() id:string): string {
+        console.log(id, 'id here')
+
         return this.booksService.getBook();
+    }
+
+    @Post('/save')
+    saveBook(@Body() body:createDto) {
+        console.log(body)
+
+        return this.booksService.saveBook(body)
     }
 }
